e5ebec87bcc0aab33ac576bc754badf05aa736d6
Yongsheng <ygong56@uwo.ca>
2024-06-27 02:24:44 -0400

    Create a user interface route and its left navbar component

---
+++ src/pages/UserInterfacePage/UserInterfacePage.jsx
@@ -0,0 +1,9 @@
+import LeftNav from "../../components/LeftNav/LeftNav";
+function UserInterfacePage(){
+    return(
+        <div className="user_interface_page">
+          <LeftNav />
+        </div>
+    )
+}
+export default UserInterfacePage;
