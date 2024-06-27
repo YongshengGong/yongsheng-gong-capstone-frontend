8b1bf2aa150c0b634c078e9d0c1126eab67d7574
Yongsheng <ygong56@uwo.ca>
2024-06-27 01:35:22 -0400

    Finish with basic structure for header, login and homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,16 @@
 import "./HomePage.scss"
-import logo from "../../assets/logo.svg"
+import Header from "../../components/Header/Header";
 function HomePage() {
-    return (
-        <>
-          <img src={logo} alt="" />
-        </>
-    )
+  return (
+    <div className="homepage">
+      <Header />
+      <main className="homepage__main">
+          <h1 className="homepage__main-title">EMS</h1>
+          <h2 className="homepage__main-subtitle">
+            Empowering the Future of Workforce Management
+          </h2>
+      </main>
+    </div>
+  )
 }
 export default HomePage;
