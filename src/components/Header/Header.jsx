95f4e4fdd32e7f49115d46b113eb2ace9bd1376c
Yongsheng <ygong56@uwo.ca>
2024-06-29 14:12:30 -0400

    Update homepage and login page styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function Header() {
     const [popup, setPopup] = useState(false);
     const handleLogin = () => {
-        setPopup(true);
+        setTimeout(()=>{setPopup(true)},200);
     }
     return (
         <>
