cc37555f01f568d69a2a617c9b8f88514adc18a0
Yongsheng <ygong56@uwo.ca>
2024-06-30 16:45:35 -0400

    Working on displaying all teams and members

---
+++ src/components/UserNav/UserNav.jsx
@@ -0,0 +1,21 @@
+import "./UserNav.scss"
+import React, { useState, useEffect } from 'react';
+import logo from "../../assets/logo.svg";
+function UserNav({ hideNav, setHideNav }) {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    const handleChildClick = (event) => {
+        event.stopPropagation();
+    };
+    return (
+        <section className={hideNav == false ? "user__main-nav" : "user__main-nav user__main-nav--hide"} onClick={() => setHideNav(true)}>
+            <nav className="user__main-nav-container" onClick={handleChildClick}>
+                <div className="user__main-nav-container-logo"><img src={logo} alt="a logo of EMS" /></div>
+                <ul className="user__main-nav-container-list">
+                    <li className="user__main-nav-container-list-item"><span>Teams</span></li>
+                    <li className="user__main-nav-container-list-item"><span>Applicants</span></li>
+                </ul>
+            </nav>
+        </section>
+    )
+}
+export default UserNav;
---
+++ src/pages/User/User.jsx
@@ -0,0 +1,21 @@
+import UserNav from "../../components/UserNav/UserNav";
+import UserTeams from "../../components/UserTeams/UserTeams";
+import UserApplicants from "../../components/UserApplicants/UserApplicants";
+import { useState } from "react";
+import "./User.scss"
+
+function User() {
+
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  const [hideNav, setHideNav] = useState(true);
+  return (
+    <div className="user">
+      <main className="user__main">
+        <UserNav hideNav={hideNav} setHideNav={setHideNav} />
+        <UserTeams setHideNav={setHideNav} />
+        {/* <UserApplicants setHideNav={setHideNav}/> */}
+      </main>
+    </div>
+  )
+}
+export default User;
